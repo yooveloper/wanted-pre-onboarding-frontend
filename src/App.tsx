3fb6cdfaa24eab80188092bf847558e1c5bc165d
yooveloper <yooveloper@gmail.com>
2023-06-21 18:52:50 +0900

    feat: 로그인 리다이렉트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 function App() {
+	const [token, setToken] = useState(false);
+
+	useEffect(() => {
+		const accessToken = localStorage.getItem('access_token');
+		if (accessToken) {
+			setToken(true);
+		}
+	}, [token]);
 	return (
 		<>
+			{token ? (
+				<Navigate to="/todos" replace />
+			) : (
+				<Navigate to="/signin" replace />
+			)}
 			<Outlet />
 		</>
 	);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+	RouterProvider,
+	createBrowserRouter,
+	Navigate
+} from 'react-router-dom';
 import NotFound from '@pages/NotFound';
 import SignIn from '@pages/SignIn';
 import SignUp from '@pages/SignUp';
@@ -10,17 +14,55 @@ import App from './App';
 
 const router = createBrowserRouter([
 	{
-		path: '/',
+		path: '',
 		element: <App />,
 		errorElement: <NotFound />,
 		children: [
-			{ index: true, element: <SignIn /> },
-			{ path: '/signin', element: <SignIn /> },
-			{ path: '/signup', element: <SignUp /> },
-			{ path: '/todos', element: <Todo /> }
+			{
+				path: '/signin',
+				element: (
+					<IsAuthRoute>
+						<SignIn />
+					</IsAuthRoute>
+				)
+			},
+			{
+				path: '/signup',
+				element: (
+					<IsAuthRoute>
+						<SignUp />
+					</IsAuthRoute>
+				)
+			},
+			{
+				path: '/todos',
+				element: (
+					<IsNotAuthRoute>
+						<Todo />
+					</IsNotAuthRoute>
+				)
+			}
 		]
 	}
 ]);
+
+function IsAuthRoute({ children }: { children: JSX.Element }) {
+	const accessToken = localStorage.getItem('access_token');
+	if (accessToken) {
+		return <Navigate to="/todos" replace />;
+	}
+
+	return children;
+}
+
+function IsNotAuthRoute({ children }: { children: JSX.Element }) {
+	const accessToken = localStorage.getItem('access_token');
+
+	if (!accessToken) {
+		return <Navigate to="/signin" replace />;
+	}
+	return children;
+}
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
