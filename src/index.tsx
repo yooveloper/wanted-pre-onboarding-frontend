1df72d70c1aa94e558e81a73d8751fc4ba78d42f
yooveloper <yooveloper@gmail.com>
2023-06-21 14:29:47 +0900

    feat route & remove lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from '@pages/NotFound';
+import SignIn from '@pages/SignIn';
+import SignUp from '@pages/SignUp';
+import Todo from '@pages/Todo';
+import { SIGNUP, TODOS } from '@constants/index';
 import App from './App';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <NotFound />,
+		children: [
+			{ index: true, element: <SignIn /> },
+			{ path: SIGNUP, element: <SignUp /> },
+			{ path: TODOS, element: <Todo /> }
+		]
+	}
+]);
+
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement,
+	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
